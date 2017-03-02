6e2fc00d65979d9f18fab31639754b7d68440eeb
delola\delola lokal <aleksandra.man@gmail.com>
2017-03-02 21:33:33 +0100

    changed textFirstHalf to firstHalfLength

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
 var animal = 'Zielone słonie';
-var animalUpperCased = animal.toUpperCase();
-var textReplace = text.replace('Papugi', animalUpperCased);
-var textFirstHalf = textReplace.length / 2;
-console.log(textReplace.slice(0, textFirstHalf));
+var textReplace = text.replace('Papugi', animal.toUpperCase());
+var firstHalfLength = textReplace.length / 2;
+console.log(textReplace.slice(0, firstHalfLength));
